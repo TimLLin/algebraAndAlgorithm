0be997f768cc5196d03cf50c4d5c0156628f9ba2
jrebeca <timur.kalimullin.87@bk.ru>
2022-10-30 04:28:06 +0300

    5

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import numpy as np
 
-base = 10000
+base = 777
 def make_matrix(graph, n):
     matrix = np.zeros((n, n), dtype='float64')
     for edge in graph:
         matrix[edge[0], edge[1]] = np.random.randint(1, base)
     return matrix
 
-def determinant(matrix):
+def det(matrix):
     n = matrix.shape[0]
-    for i in range(0, n - 1):
+    matrix = matrix.tolist()
+    for i in range(n):
+        if matrix[i][i] == 0:
+            j = next((j for j in range(i + 1, n) if matrix[j][i] != 0), None)
+            if j is None:
+                return False
+            matrix[i], matrix[j] = matrix[j], matrix[i]
         for j in range(i + 1, n):
-            if matrix[j, i] != 0.0:
-                curr = (matrix[j, i] / matrix[i, i]) % base
-                matrix[j, i+1:n] = (matrix[j, i+1:n] - curr * matrix[i, i+1:n]) % base
-
-    diag = np.diag(matrix)
-    return np.prod(diag) % base
+            factor = (matrix[j][i] / matrix[i][i]) % base
+            for k in range(i + 1, n):
+                matrix[j][k] = (matrix[j][k] - factor * matrix[i][k]) % base
+    return True
 
 n = int(input())
 graph = []
@@ -28,12 +32,10 @@ for _ in range(n):
     if curr_max > size:
         size = curr_max
 
-
 matrix = make_matrix(graph, size+1)
 
-det = determinant(matrix)
-
-if det != 0:
+cur_det = det(matrix)
+if cur_det:
     print('yes')
-elif det == 0:
+else:
     print('no')
